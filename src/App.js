a829258b07781ffae47558404c48bd5011274cc4
Gourav199 <tewarygourav1997@gmail.com>
2022-12-05 09:29:05 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import Header from './component/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import SearchFilter from './component/searchFilter';
+
 import Table from './component/Table';
-import axios from 'axios';
-import { useState, useEffect } from 'react';
+
 
 function App() {
   
